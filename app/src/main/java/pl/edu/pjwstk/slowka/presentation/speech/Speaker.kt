13942f4ecdf054dba817ae6a5e7c8c24e57909cb
Jan Gonera <jgn@touk.pl>
2016-09-10 13:40:53 +0200

    [feature/16-ivona] progress fixes

--- app/src/main/java/pl/edu/pjwstk/slowka/presentation/speech/Speaker.kt
+++ app/src/main/java/pl/edu/pjwstk/slowka/presentation/speech/Speaker.kt
@@ -5,35 +5,46 @@ import android.speech.tts.TextToSpeech
 import android.speech.tts.UtteranceProgressListener
 import java.util.*
 
-class Speaker(private val context: Context) {
+class Speaker(private val context: Context): UtteranceProgressListener() {
 
     val speech: TextToSpeech
+
+    var onStart: (()->Unit)? = null
+
+    var onFinish: (()->Unit)? = null
 
     init {
         speech = TextToSpeech(context, { initStatus -> })
         speech.language = Locale("pl")
         speech.setSpeechRate(0.8f)
     }
-
     fun speak(textToSpeak: String) {
         speak(textToSpeak, null, null)
     }
-
     fun speak(textToSpeak: String, onStart: (()->Unit)?, onFinish: (()->Unit)?) {
-        speech.setOnUtteranceProgressListener(object: UtteranceProgressListener() {
-
-            override fun onDone(utteranceId: String?) {
-                onFinish?.invoke()
-                speech.setOnUtteranceProgressListener(null)
-            }
-
-            override fun onError(utteranceId: String?) { }
-
-            override fun onStart(utteranceId: String?) {
-                onStart?.invoke()
-            }
-        })
+        finishSpeech()
+        this.onStart = onStart
+        this.onFinish = onFinish
+        speech.setOnUtteranceProgressListener(this)
         speech.speak(textToSpeak, TextToSpeech.QUEUE_FLUSH, null, Random().nextInt().toString())
     }
 
+    override fun onStart(utteranceId: String?) {
+        onStart?.invoke()
+    }
+
+    override fun onError(utteranceId: String?) {
+        onFinish?.invoke()
+    }
+
+    override fun onDone(utteranceId: String?) {
+        onFinish?.invoke()
+    }
+
+    private fun finishSpeech() {
+        onFinish?.invoke()
+        speech.setOnUtteranceProgressListener(null)
+        onStart = null
+        onFinish = null
+    }
 }
